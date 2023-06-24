252a2368827d1ed3cea4ba21566ee596ebfc4dfb
Inu-Rei <ronaldramossierra@hotmail.com>
2023-06-24 01:00:07 -0700

    actualizacion de nav : logo, input buscar y el boton de busqueda

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { PokemonContext } from '../context/PokemonContext';
 import logo from '../assets/Pokédex_logo.png'
+import Botones from '../Botones/Botones';
 
 export const Navigation = () => {
 	const { onInputChange, valueSearch, onResetForm } =
@@ -21,16 +22,21 @@ export const Navigation = () => {
 	return (
 		<>
 			<header className='container'>
-				<Link to='/' className='logo'>
+				
+
+				<form onSubmit={onSearchSubmit}>
+					<div className='form-group'>
+						
+						
+					</div>
+					<nav className='navbar'>
+					<Link to='/' className='logo'>
 					<img
 						src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png'
 						alt='Logo Pokedex'
 					/>
 				</Link>
-
-				<form onSubmit={onSearchSubmit}>
-					<div className='form-group'>
-						<svg
+				<svg
 							xmlns='http://www.w3.org/2000/svg'
 							fill='none'
 							viewBox='0 0 24 24'
@@ -44,17 +50,16 @@ export const Navigation = () => {
 								d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
 							/>
 						</svg>
-						<input
+					<input className='barra-busqueda'
 							type='search'
 							name='valueSearch'
 							id=''
 							value={valueSearch}
 							onChange={onInputChange}
-							placeholder='Buscar nombre de pokemon'
-						/>
-					</div>
-
+							placeholder='Buscar nombre de pokemon'/>
 					<button className='btn-search'>Buscar</button>
+					
+					</nav>
 				</form>
 			</header>
 
